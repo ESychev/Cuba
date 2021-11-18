105f4b6de03c2059a8b7a2175b0a18bc031149e6
ESychev <375298357706@ya.ru>
2021-11-18 16:59:46 +0300

    add adaptive

--- js/main.js
+++ js/main.js
@@ -4,5 +4,7 @@ $(function() {
         prevArrow: '<button type = "button" class = "slick-prev">  <img src="images/arrow-left.svg" alt=""> </ button>',
         nextArrow: '<button type = "button" class = "slick-next">  <img src="images/arrow-right.svg" alt=""> </ button>'
     });
-    $('.menu__btn').on('click', function() {});
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
+    });
 });
